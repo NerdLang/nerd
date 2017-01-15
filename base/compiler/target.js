adaeb62e931e6835df545894a3dce1b92b42383c
Adrien THIERRY <adrien.thierryfr@gmail.com>
2017-01-15 18:09:41 +0100

    Added linux-arm32v7

--- base/compiler/target.js
+++ base/compiler/target.js
@@ -1,2 +1,2 @@
-var target = ["win-x86-32", "win-x86-64", "linux-x86-32", "linux-x86-64"];
+var target = ["win-x86-32", "win-x86-64", "linux-x86-32", "linux-x86-64", "linux-arm32v7"];
 module.exports = target;
