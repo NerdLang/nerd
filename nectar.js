cf95873882f366dd821cb92b2f2af89da9194bd8
Adrien THIERRY <adrien.thierryfr@gmail.com>
2017-03-18 11:24:37 +0100

    Updated to 0.0.27

--- nectar.js
+++ nectar.js
@@ -27,7 +27,7 @@
  *
  */
 
-var VERSION = "0.0.26";
+var VERSION = "0.0.27";
 
 var fs = require('fs');
 var os = require('os');
@@ -453,6 +453,7 @@ function Build(prepare)
             if(result.success == false)
             {
               console.log(result.message);
+	      process.exit(1);
             }
             else
             {
