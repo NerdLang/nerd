71b78cc882c30b8747c5b5ca78f87be9d7162803
Adrien THIERRY <adrien-thierry@users.noreply.github.com>
2018-01-05 10:18:28 +0100

    Update

--- example/c/test.c
+++ example/c/test.c
@@ -2,10 +2,10 @@
 var test()
 {
   printf("IN C\n");
-  return __N_Create_String("C TO JS");
+  return __N_Create_String("C TO JS", "__return__");
 }
 
 var entier()
 {
-  return __N_Create_Int(42);
+  return __N_Create_Int(42, "__return__");
 }
