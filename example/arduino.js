7f45edbb7553260317895f8d4c9a40e7c2718846
adrien <adrien@ghost.seraum.local>
2018-01-05 16:36:17 +0100

    Updated arduino.js example

--- example/arduino.js
+++ example/arduino.js
@@ -1,11 +1,15 @@
 var arduino = require("arduino");
 
-var ledPin = 11;
-arduino.pinMode(ledPin, arduino.OUTPUT);
+var ledPin = 11; // LED PIN
+var low = arduino.LOW;
+var high = arduino.HIGH;
+
+arduino.pinMode(ledPin, arduino.OUTPUT); // Set pin on OUTPUT mode
+
 while(1)
 {
-	arduino.digitalWrite(ledPin, 1);
-	arduino.delay(1);
-	arduino.digitalWrite(ledPin, 0);
-	arduino.delay(1);
+	arduino.digitalWrite(ledPin, high); // ON
+	arduino.delay(1000);
+	arduino.digitalWrite(ledPin, low); // OFF
+	arduino.delay(1000);
 }
