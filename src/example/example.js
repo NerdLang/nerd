1c98252297a935445173238cca07e5349203b3c2
hitmands - Giuseppe Mandato <g.mandato@elsevier.com>
2018-01-19 13:56:35 +0000

    feat(ci): tests and coverage

---
+++ src/example/example.js
@@ -0,0 +1,11 @@
+export const sum = (...nums) => nums.reduce((a, b) => a + b)
+
+export function * fibonacci () {
+  let [current, next] = [0, 1]
+
+  while (true) {
+    yield [current, next];
+
+    [current, next] = [next, current + next]
+  }
+}
