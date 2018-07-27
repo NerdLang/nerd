a99e298fe09fc3584cfa417a51f7112061388aae
Adrien THIERRY <adrien@Mac-mini-de-Adrien.local>
2018-07-27 10:39:14 +0200

    Updated

--- example/bool.js
+++ example/bool.js
@@ -1,3 +1,11 @@
 var test = true;
-console.log(typeof test);
-console.log(test);
+console.log("Test is : " + test + " | and type -> " + typeof test);
+
+if(test)
+{
+  console.log("True !");
+}
+else
+{
+  console.log("False ...");
+}
