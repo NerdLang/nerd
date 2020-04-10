ecaebf04482ca5c7d716cc4fd5d84546956f9a9c
Adrien FR <adrien.thierryfr@gmail.com>
2020-04-10 13:29:50 +0200

    Updated bench_array

--- example/bench_array.js
+++ example/bench_array.js
@@ -1,4 +1,4 @@
-var _loop = 1000000;
+var _loop = 1000;
 var _arr = __NJS_Create_Array();
 
 var _res = 0;
@@ -8,9 +8,9 @@ for(var i = 0; i < _loop; i++)
 	_arr[i] = i;
 }
 
-for(var i = 0; i < 10000; i++)
+for(var i = 0; i < 1000; i++)
 {
 	_res += _arr[i];
 }
-_arr[1] = 1;
-console.log(_arr[1]);
+
+console.log(_res);
