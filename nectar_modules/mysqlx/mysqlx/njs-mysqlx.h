059f4a767855512855651185f4c31059aa5e887a
Adrien Thierry <adrien.thierryfr@gmail.com>
2020-07-08 10:14:37 +0000

    Updated mysqlx module

--- nectar_modules/mysqlx/mysqlx/njs-mysqlx.h
+++ nectar_modules/mysqlx/mysqlx/njs-mysqlx.h
@@ -61,53 +61,81 @@ function __NJS_NATIVE_CONNECT_MYSQL(_auth, _schema, _coll)
    function _mysql_modify(_obj)
    {
      "SCOPED_FUNCTION";
-     var _search = __NJS_Object_Get("search",_obj);
-     if(_search.type != __NJS_STRING) return __NJS_Create_Boolean(0);
+     try
+      {
+        var _search = __NJS_Object_Get("search",_obj);
+        if(_search.type != __NJS_STRING) return __NJS_Create_Boolean(0);
 
         auto _cm = coll.modify(__NJS_Get_String(_search));
 
         var _set = __NJS_Object_Get("set", _obj);
-        if(_set && _set.type == __NJS_ARRAY)
-        {
-          var _left = __NJS_Object_Get(0, _set);
-          var _right = __NJS_Object_Get(1, _set);
-          if(_left.type == __NJS_STRING && _right.type == __NJS_STRING)
-          _cm.set(__NJS_Get_String(_left), __NJS_Get_String(_right));
-        }
+        if(_set && _set.type == __NJS_ARRAY)                                                                                   
+        {                                                                                                                                                                                                                                               
+          for(int i = 0; i < _set.get().a->__NJS_VALUE.size(); i+=2)                                                             
+          {                                                                                                                            
+            if(i+1 <= _set.get().a->__NJS_VALUE.size())                                                                            
+            {                                                                                                      
+              var _left = __NJS_Object_Get(i, _set);                                                                                 
+              var _right = __NJS_Object_Get(i+1, _set);
+              if(_left.type == __NJS_STRING && _right.type == __NJS_STRING)                                                          
+              _cm.set(__NJS_Get_String(_left), __NJS_Get_String(_right));
+            }                                                                                                                
+          }
+        }                                                                                                                      
         _cm.execute();
 
         var _result = __NJS_Create_Boolean(1);
         return _result;
+      }
+      catch(const std::exception& e)
+      {
+        return __NJS_Create_Boolean(0);
+      }
    }
 
     function _mysql_find(_request)
     {
       "SCOPED_FUNCTION";
-      var _result = __NJS_Create_Array();
-      std::list<DbDoc> dbList;
-      if(_request) dbList = coll.find(__NJS_Get_String(_request)).execute().fetchAll();
-      else dbList = coll.find().execute().fetchAll();
-      
-      int i = 0;
-      std::list<DbDoc>::iterator it;
-      stringstream s;
-      s << "{\"data\":[";
-      for (it = dbList.begin(); it != dbList.end(); it++)
+      try
       {
-        if(i) s << ",";
-        it->print(s);
-        i++;
+        var _result = __NJS_Create_Array();
+        std::list<DbDoc> dbList;
+        if(_request) dbList = coll.find(__NJS_Get_String(_request)).execute().fetchAll();
+        else dbList = coll.find().execute().fetchAll();
+        
+        int i = 0;
+        std::list<DbDoc>::iterator it;
+        stringstream s;
+        s << "{\"data\":[";
+        for (it = dbList.begin(); it != dbList.end(); it++)
+        {
+          if(i) s << ",";
+          it->print(s);
+          i++;
+        }
+        s << "], \"code\":0, \"length\":" << dbList.size() << "}";
+        std::string _str = std::string(s.str());
+        var _data = _str.c_str();
+        return _data;
       }
-      s << "], \"code\":0, \"length\":" << dbList.size() << "}";
-      std::string _str = std::string(s.str());
-      var _data = _str.c_str();
-      return _data;
+      catch(const std::exception& e)
+      {
+        return '{"code:"1, "msg":"Find error"}';
+      }
     }
     
     function _mysql_remove(_request)
     {
       "SCOPED_FUNCTION";
-      coll.remove(__NJS_Get_String(_request)).execute();
+      try 
+      {
+        coll.remove(__NJS_Get_String(_request)).execute();
+        return __NJS_Create_Boolean(1);
+      }
+      catch(const std::exception& e)
+      {
+        return __NJS_Create_Boolean(0);
+      }
     }
 
     __NJS_Object_Set("version", "8", _mysqlObject);
