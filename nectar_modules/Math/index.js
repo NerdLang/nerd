b3f80ce360eb812b4812d5f7ccd77e0fbfd92659
Saiv46 <47699394+Saiv46@users.noreply.github.com>
2020-08-06 16:45:07 +0800

    Math early implementation (WIP)

--- nectar_modules/Math/index.js
+++ nectar_modules/Math/index.js
@@ -1,32 +1,72 @@
-/* Math std module */
+"!_ffi_include lib/math.h";
 
 var _Math = 
 {
     /*** PROPERTIES ***/
     
-    E: 2.718,
-    LN2: 2.302,
-    LOG2E: 1.442,
-    LOG10E: 0.434,
-    PI: 3.141592653589793,
-    SQRT1_2: 0.707,
-    SQRT2: 1.414,
+    E: __NJS_M_E(),
+    LN2: __NJS_M_LN2(),
+    LOG2E: __NJS_M_LOG2E(),
+    LOG10E: __NJS_M_LOG10E(),
+    PI: __NJS_M_PI(),
+    SQRT1_2: __NJS_M_SQRT1_2(),
+    SQRT2: __NJS_M_SQRT2(),
 
     /*** END PROPERTIES ***/
 
     /*** METHODS ***/
 
-    abs: function(i)
+    //abs: __NJS_M_ABS,
+	abs: function(i)
     {
         if(i < 0) return -i;
         return i;
     },
+	acos: __NJS_M_ACOS,
+	acosh: __NJS_M_ACOSH,
+	asin: __NJS_M_ASIN,
+	asinh: __NJS_M_ASINH,
+	atan: __NJS_M_ATAN,
+	atanh: __NJS_M_ATANH,
+	atan2: __NJS_M_ATAN2,
+	cbrt: __NJS_M_CBRT,
+	ceil: __NJS_M_CEIL,
+	clz32: __NJS_M_CLZ32,
+	cos: __NJS_M_COS,
+	cosh: __NJS_M_COSH,
+	exp: __NJS_M_EXP,
+	expm1: __NJS_M_EXPM1,
+	floor: __NJS_M_FLOOR,
+	fround: __NJS_M_FROUND,
+    //hypot: __NJS_M_HYPOT,
+    hypot: function (_array) {
+        var max = 0;
+        var s = 0;
+        for (var i = 0; i < _array.length; i += 1) {
+            if(typeof _array[i] == "number")
+            {
+                var arg = Math.abs(Number(_array[i]));
+                if (arg > max) {
+                    s *= (max / arg) * (max / arg);
+                    max = arg;
+                }
+                s += arg === 0 && max === 0 ? 0 : (arg / max) * (arg / max);
+            }
+        }
+        return max === 1 / 0 ? 1 / 0 : max * Math.sqrt(s);
+    },
+    //imul: __NJS_M_IMUL,
     imul: function(x, y)
     {
         var _res = 0;
         _res = (x * y);
         return _res;
     },
+	log: __NJS_M_LOG,
+	log1p: __NJS_M_LOG1P,
+	log10: __NJS_M_LOG10,
+	log2: __NJS_M_LOG2,
+	//max: __NJS_M_MAX,
     max: function(_array)
     {
         var _res;
@@ -41,6 +81,7 @@ var _Math =
         if(typeof _res == "undefined") _res = 0;
         return _res;
     },
+    //min: __NJS_M_MIN,
     min: function(_array)
     {
         var _res;
@@ -55,27 +96,19 @@ var _Math =
         if(typeof _res == "undefined") _res = 0;
         return _res;
     },
-    pow: function(i, j)
-    {
-        if(j == 0) return 1;
-        else if(j > 0)
-        {
-            j--;
-            while(j)
-            {
-                i*=i;
-                j--;
-            }
-        }
-        else if(j < 0)
-        {
-            i = 1 / _Math.pow(i, _Math.abs(j));
-        }
-        return i;
-    },
+    pow: __NJS_M_POW,
+	random: __NJS_M_RANDOM,
+	round: __NJS_M_ROUND,
+	sign: __NJS_M_SIGN,
+	sin: __NJS_M_SIN,
+	sinh: __NJS_M_SINH,
+	sqrt: __NJS_M_SQRT,
+	tan: __NJS_M_TAN,
+	tanh: __NJS_M_TANH,
+    trunc: __NJS_M_TRUNC,
     
     /*** END METHODS ***/
-
+    toString: function () { return "[object Math]"; }
 };
 
 
---
+++ nectar_modules/Math/lib/math.h
@@ -0,0 +1,78 @@
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <string>
+
+using namespace std;
+
+function __NJS_M_E() { return __NJS_VAR(M_E); };
+function __NJS_M_LN2() { return __NJS_VAR(M_LN2); };
+function __NJS_M_LOG2E() { return __NJS_VAR(M_LOG2E); };
+function __NJS_M_LOG10E() { return __NJS_VAR(M_LOG10E); };
+function __NJS_M_PI() { return __NJS_VAR(M_PI); };
+function __NJS_M_SQRT1_2() { return __NJS_VAR(M_SQRT1_2); };
+function __NJS_M_SQRT2() { return __NJS_VAR(M_SQRT2); };
+
+function __GET_VALUE (_var)
+{
+	if (_var.type == __NJS_NUMBER) {
+		return _var.get().i;
+	} else if (_var.type == __NJS_DOUBLE) {
+		return _var.get().d;
+	} else if (_var.type == __NJS_STRING) {
+		return stod(_var.get().s);
+	}
+}
+
+function __NJS_MATH_ABS(_var) { return __NJS_VAR(abs(__GET_VALUE(_var))); };
+function __NJS_MATH_ACOS(_var) { return __NJS_VAR(acos(__GET_VALUE(_var))); };
+function __NJS_MATH_ACOSH(_var) { return __NJS_VAR(acosh(__GET_VALUE(_var))); };
+function __NJS_MATH_ASIN(_var) { return __NJS_VAR(asin(__GET_VALUE(_var))); };
+function __NJS_MATH_ASINH(_var) { return __NJS_VAR(asinh(__GET_VALUE(_var))); };
+function __NJS_MATH_ATAN(_var) { return __NJS_VAR(atan(__GET_VALUE(_var))); };
+function __NJS_MATH_ATAN2(_var) { return __NJS_VAR(atan2(__GET_VALUE(_var))); };
+function __NJS_MATH_ATANH(_var) { return __NJS_VAR(atanh(__GET_VALUE(_var))); };
+function __NJS_MATH_CBRT(_var) { return __NJS_VAR(cbrt(__GET_VALUE(_var))); };
+function __NJS_MATH_CEIL(_var) { return __NJS_VAR(ceil(__GET_VALUE(_var))); };
+function __NJS_MATH_CLZ32(_var) {
+	auto x = (int)__GET_VALUE(_var);
+    if (x == 0) {
+      return __NJS_VAR(32);
+    }
+    return __NJS_VAR((int)(31 - log(x >> 0) / M_LN2));
+};
+function __NJS_MATH_COS(_var) { return __NJS_VAR(cos(__GET_VALUE(_var))); };
+function __NJS_MATH_COSH(_var) { return __NJS_VAR(cosh(__GET_VALUE(_var))); };
+function __NJS_MATH_EXP(_var) { return __NJS_VAR(exp(__GET_VALUE(_var))); };
+function __NJS_MATH_EXPM1(_var) { return __NJS_VAR(expm1(__GET_VALUE(_var))); };
+function __NJS_MATH_FLOOR(_var) { return __NJS_VAR((int)floor(__GET_VALUE(_var))); };
+function __NJS_MATH_FROUND(_var) { return __NJS_VAR((float)__GET_VALUE(_var))); };
+//function __NJS_MATH_HYPOT(_var) {
+//	return __NJS_VAR(cos(__GET_VALUE(_var)));
+//};
+//function __NJS_MATH_IMUL(_var) {
+//	return __NJS_VAR(imul(__GET_VALUE(_var)));
+//};
+function __NJS_MATH_LOG(_var) { return __NJS_VAR(log(__GET_VALUE(_var))); };
+function __NJS_MATH_LOG1P(_var) { return __NJS_VAR(log1p(__GET_VALUE(_var))); };
+function __NJS_MATH_LOG2(_var) { return __NJS_VAR(log2(__GET_VALUE(_var))); };
+function __NJS_MATH_LOG10(_var) { return __NJS_VAR(log10(__GET_VALUE(_var))); };
+//function __NJS_MATH_MAX(_var) {	
+//	return __NJS_VAR(log2(__GET_VALUE(_var)));
+//};
+//function __NJS_MATH_MIN(_var) {
+//	return __NJS_VAR(log2(__GET_VALUE(_var)));
+//};
+function __NJS_MATH_POW(_var, _var2) { return __NJS_VAR(pow(__GET_VALUE(_var), __GET_VALUE(_var2))); };
+function __NJS_MATH_RANDOM() { return __NJS_VAR(static_cast<float>(rand()) / static_cast<float>(RAND_MAX)); };
+function __NJS_MATH_ROUND(_var) { return __NJS_VAR((int)round(__GET_VALUE(_var))); };
+function __NJS_MATH_SIGN(_var) {
+	auto x = __GET_VALUE(_var);
+	if (x == 0) { return __NJS_VAR(0); }
+	else { return __NJS_VAR(x > 0 ? 1 : -1); }
+};
+function __NJS_MATH_SIN(_var) { return __NJS_VAR(sin(__GET_VALUE(_var))); };
+function __NJS_MATH_SINH(_var) { return __NJS_VAR(sinh(__GET_VALUE(_var))); };
+function __NJS_MATH_SQRT(_var) { return __NJS_VAR(sqrt(__GET_VALUE(_var))); };
+function __NJS_MATH_TAN(_var) { return __NJS_VAR(tan(__GET_VALUE(_var))); };
+function __NJS_MATH_TANH(_var) { return __NJS_VAR(tanh(__GET_VALUE(_var))); };
+function __NJS_MATH_TRUNC(_var) { return __NJS_VAR((int)(__GET_VALUE(_var))); };
