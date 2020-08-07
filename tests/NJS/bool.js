1a0d43544afe37c80677040140ee86b19b110cfd
Saiv46 <47699394+Saiv46@users.noreply.github.com>
2020-08-08 05:40:55 +0800

    Updated bool.js

--- tests/NJS/bool.js
+++ tests/NJS/bool.js
@@ -1,12 +1,12 @@
-var test = true;
 
-console.log("Test is : " + test + " | and type -> " + typeof test);
+var _true = true;
+var _true2 = !!1;
+var _true3 = !!"1";
 
-if(test)
-{
-  console.log("True !");
-}
-else
-{
-  console.log("False ...");
-}
+var _false = false;
+var _false2 = !!0;
+var _false3 = !!"";
+
+console.log("Boolean : " + _true + " | " + _false)
+console.log("Number : " + _true2 + " | " + _false2)
+console.log("String : " + _true3 + " | " + _false3)
