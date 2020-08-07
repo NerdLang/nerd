82f0e82d21dc7d614a7f377d333d8c41827a5cb6
Saiv46 <47699394+Saiv46@users.noreply.github.com>
2020-08-08 03:43:16 +0800

    Create random.js

---
+++ example/random.js
@@ -0,0 +1,4 @@
+console.log("Math.random(): " + Math.random())
+console.log("Math.random() * 2: " + Math.random() * 2)
+console.log("Math.random() * 255: " + Math.random() * 255)
+console.log("Math.random() * 10000: " + Math.random() * 10000)
