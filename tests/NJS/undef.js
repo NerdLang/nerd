b388c5526502d147729a118bd548db565446794d
Adrien THIERRY <adrien.thierryfr@gmail.com>
2020-08-10 17:44:29 +0200

    Add tests

---
+++ tests/NJS/undef.js
@@ -0,0 +1,12 @@
+undef = "ok"
+
+for(i = 0; i < 1; i++)
+{
+	console.log("undef i loop ok");
+}
+
+oUnd.test = 1;
+aUnd["test"] = 1;
+aIntUnd[0] = 1;
+
+console.log(undef);
---
+++ tests/NJS/void.js
@@ -0,0 +1,6 @@
+console.log(void 0);
+
+var a = void 0;
+console.log(a);
+
+void (function(){console.log("ok"); return 1;})();
