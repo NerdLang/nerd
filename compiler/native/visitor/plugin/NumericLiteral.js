57ffe747e2e59a26498af8ef038ee7408b713fc0
Adrien THIERRY <adrien.thierryfr@gmail.com>
2020-09-29 16:48:37 +0200

    Update BinaryExpression

--- compiler/native/visitor/plugin/NumericLiteral.js
+++ compiler/native/visitor/plugin/NumericLiteral.js
@@ -22,10 +22,20 @@
  
 function NumericLiteral(_path)
 {
-	if(_path.parent.type == "MemberExpression" || _path.parent.type == "BinaryExpression")
+	if(_path.parent.type == "MemberExpression")
 	{
 		_path.replaceWithSourceString("__NJS_VAR(" + _path.node.extra.raw + ")");
 		_path.skip();
+	}
+	else if(_path.parent.type == "BinaryExpression")
+	{
+		var start = _path.node.start;
+		var end = _path.node.end;
+		if(_path.parent.left.start == start && _path.parent.left.end == end)
+		{
+			_path.replaceWithSourceString("__NJS_VAR(" + _path.node.extra.raw + ")");
+			_path.skip();
+		}
 	}
 }
 module.exports = NumericLiteral;
