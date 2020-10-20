5f77922c4419c1ef21a8007681f17a979aad6a2e
Adrien THIERRY <adrien.thierryfr@gmail.com>
2020-10-20 12:38:40 +0200

    Enable redeclare variables

--- compiler/native/env/std.js
+++ compiler/native/env/std.js
@@ -42,7 +42,7 @@ var STD =
             "indent": "off",
             "linebreak-style": "off",
             "no-unused-vars": ["warn", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }],
-            "no-redeclare": ["error", { "builtinGlobals": false }],
+            //"no-redeclare": ["error", { "builtinGlobals": false }],
 			"no-const-assign": "error",
         },
         "globals":
