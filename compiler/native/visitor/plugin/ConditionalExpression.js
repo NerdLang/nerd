693ccaa46ae4aada9b195bae15bc4cf8e6a48137
Adrien THIERRY <adrien.thierryfr@gmail.com>
2020-10-25 17:07:34 +0100

    Update ternary expression support

--- compiler/native/visitor/plugin/ConditionalExpression.js
+++ compiler/native/visitor/plugin/ConditionalExpression.js
@@ -24,13 +24,22 @@ function ConditionalExpression(_path)
 {
 	if(_path.node.consequent.type != _path.node.alternate.type)
 	{
-		if(_path.node.alternate.type == "NumericLiteral" || _path.node.alternate.type == "StringLiteral")
+		
+		if(_path.node.alternate.type == "NumericLiteral" || _path.node.alternate.type == "StringLiteral" || _path.node.alternate.type == "UnaryExpression")
 		{
 			var _rnd = "__NJS_TERNARY" + RND();
 			COMPILER.DECL.push(`var ${_rnd} = ${_path.node.alternate.value}`);
 			_path.node.alternate.type = "Identifier";
 			_path.node.alternate.name = _rnd;
 		}
+		if(_path.node.consequent.type == "NumericLiteral" || _path.node.consequent.type == "StringLiteral" || _path.node.consequent.type == "UnaryExpression")
+		{
+			var _rnd = "__NJS_TERNARY" + RND();
+			COMPILER.DECL.push(`var ${_rnd} = ${_path.node.consequent.value}`);
+			_path.node.consequent.type = "Identifier";
+			_path.node.consequent.name = _rnd;
+		}
+		
 	}
 }
 module.exports = ConditionalExpression;
